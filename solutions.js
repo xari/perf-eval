7ea4fd510b2884ec3d6c966a2ef8b63603998f01
xari <hrtanderson@gmail.com>
2021-10-08 23:32:38 +0200

    Upgraded testing functions for more detailed and organized logging Also adds more algorithm solutions for other challenges.

--- solutions.js
+++ solutions.js
@@ -1,10 +1,17 @@
-function solution_reduce(A) {
+// Codility challenge solutions from my week practice week.
+// Unit tests and benchmark tests can be found in solutions.test.js
+
+// =============================================
+//
+// Find the first integer that isn't in the array.
+
+export function find_missing_int_reduce(A) {
   return A.filter((n) => n > 0)
     .sort((a, b) => a - b)
     .reduce((prev, n, i) => (n === prev ? n + 1 : prev), 1);
 }
 
-function solution_reduce_with_eject(A) {
+export function find_missing_int_reduce_with_eject(A) {
   return A.filter((n) => n > 0)
     .sort((a, b) => a - b)
     .reduce((prev, n, i, arr) => {
@@ -17,7 +24,7 @@ function solution_reduce_with_eject(A) {
     }, 1);
 }
 
-function solution_forEach(A) {
+export function find_missing_int_forEach(A) {
   let x = 1;
 
   A.filter((n) => n > 0)
@@ -34,7 +41,7 @@ function solution_forEach(A) {
 
 // This solution was taken from the StackOverflow answer below:
 // https://stackoverflow.com/a/56969919
-function solution_for(A) {
+export function find_missing_int_for(A) {
   A = A.filter((x) => x >= 1).sort((a, b) => a - b);
 
   let x = 1;
@@ -50,7 +57,7 @@ function solution_for(A) {
   return x;
 }
 
-function solution_for_of(A) {
+export function find_missing_int_for_of(A) {
   A = A.filter((x) => x >= 1).sort((a, b) => a - b);
 
   let x = 1;
@@ -65,10 +72,294 @@ function solution_for_of(A) {
   return x;
 }
 
-export default [
-  solution_for,
-  solution_forEach,
-  solution_reduce,
-  solution_reduce_with_eject,
-  solution_for_of,
-];
+// =============================================
+//
+// Recursively replace all abb with baa
+
+export function recursively_regex(S) {
+  return S.match("abb") === null ? S : solution(S.replace(/abb/g, "baa"));
+}
+
+export function while_replace_regex(S) {
+  let newS = S;
+
+  while (newS.match("abb")) {
+    newS = newS.replace(/abb/g, "baa");
+  }
+
+  return newS;
+}
+
+// =============================================
+//
+// Find the largest binary gap length of an integer
+
+export function find_gap_length(N) {
+  const gapLengths = (N >>> 0) // Convert to binary
+    .toString(2) // Convert to binary
+    .replace(/^0+|0+$/g, "") // Remove 0s on either end
+    .split("1") // get the binary gaps (just the 0s)
+    .filter(Boolean) // Remove the empty strings
+    .map((gap) => gap.length); // (ex. "000" => 3)
+
+  return gapLengths.length
+    ? gapLengths.reduce((acc, cur) => (cur > acc ? cur : acc))
+    : 0;
+}
+
+// =============================================
+//
+// Rotate an array K times
+
+export function rotate_array(A, K) {
+  if (A.length > 0) {
+    for (let i = 0; i < K; i++) {
+      A.unshift(A.pop());
+    }
+  }
+
+  return A;
+}
+
+// =============================================
+//
+// Frog step... (How long until all steps appear?)
+
+export function find_earliest_completion(X, A) {
+  const all_steps = Array.from({ length: X }, (_, i) => i + 1);
+
+  const sorted = A.map((n, i) => ({ n: n, i: i }))
+    .filter((step) => step.n <= X)
+    .sort((step1, step2) => step2.i - step1.i); // Sort by reversed index order... This reversed order is importand because the way that we create the unique values array below will simply look for the last instance of the value in the array.
+
+  const unique = [...new Map(sorted.map((item) => [item["n"], item])).values()];
+
+  return all_steps.every(
+    (step) => unique.map((withI) => withI.n).indexOf(step) !== -1
+  )
+    ? unique.reduce((acc, step) => (step.n === X ? step.i : acc))
+    : -1;
+}
+
+// =============================================
+//
+// Count the bounded slices of an array... This solution is O(N ** 3)
+
+export function count_bounded_slices(K, A) {
+  const bounded_slices = [];
+
+  for (let i = 0; i < A.length; i++) {
+    let j = i + 1;
+
+    while (j <= A.length) {
+      const slice = A.slice(i, j);
+      const min = Math.min(...slice);
+      const max = Math.max(...slice);
+
+      if (Math.abs(min - max) <= K) {
+        bounded_slices.push([i, j]);
+      } else {
+        break;
+      }
+
+      j++;
+    }
+  }
+
+  return bounded_slices.length;
+}
+
+// =============================================
+//
+// Tree traversal to return maximum tree height
+
+export function get_tree_height(T) {
+  const queue = T == null ? [] : [{ t: T, i: 0 }];
+  const heights = [];
+
+  while (queue.length) {
+    const current = queue.shift();
+
+    heights.push(current.i);
+
+    if (current.t.l) {
+      queue.push({ t: current.t.l, i: current.i + 1 });
+    }
+
+    if (current.t.r) {
+      queue.push({ t: current.t.r, i: current.i + 1 });
+    }
+  }
+
+  return T == null ? -1 : Math.max(...heights);
+}
+
+// =============================================
+//
+// Incomplete: convert integers to strings of As and Bs
+// This one has an issue with the rounding.... It doesn't account for uneven leftover amounts of letters... It should be fixed to be able to concat either more or less than the default number of letters, depending on how many of the other numbers are left to be popped.
+
+export function get_a_b_string(A, B) {
+  const As = "a".repeat(A).split("");
+  const Bs = "b".repeat(B).split("");
+  const letters = A >= B ? [...As, ...Bs] : [...Bs, ...As];
+  const spliceThisMany = Math.min(
+    2,
+    Math.round(Math.max(A, B) / Math.min(A, B))
+  );
+
+  let resultString = "";
+
+  while (letters.length) {
+    resultString = resultString.concat(
+      letters.splice(0, spliceThisMany).join("")
+    );
+
+    const popped = letters.pop();
+
+    // Check first in case there aren't any leftover letters.
+    popped ? (resultString = resultString.concat(popped)) : null;
+  }
+
+  return resultString;
+}
+
+// =============================================
+//
+// Inversion counting
+
+export function count_inversions_bubble(A) {
+  let inversionCount = 0;
+  let clear = false;
+
+  while (clear !== true) {
+    clear = true;
+
+    for (let i = 0; i < A.length; i++) {
+      const slice = A.slice(i, i + 2);
+
+      if (slice[0] > slice[1]) {
+        A.splice(i, 2, ...slice.reverse());
+        clear = false;
+        inversionCount = inversionCount + 1;
+      }
+    }
+  }
+
+  let i = 0;
+  let queue = [A.slice(i, i + 2)];
+
+  while (queue.length) {
+    if (slice[0] > slice[1]) {
+      A.splice(i, 2, ...slice.reverse());
+    }
+
+    i++;
+    queue.push([A.slice(i, i + 2)]);
+  }
+
+  return inversionCount > 1000000000 ? -1 : inversionCount;
+}
+
+export function count_inversions_bubble_queue(A) {
+  let inversionCount = 0;
+  let i = 0;
+  let queue = [A.slice(i, i + 2)];
+
+  while (queue.length) {
+    if (slice[0] > slice[1]) {
+      A.splice(i, 2, ...slice.reverse());
+    }
+
+    i++;
+    queue.push([A.slice(i, i + 2)]);
+  }
+
+  return inversionCount > 1000000000 ? -1 : inversionCount;
+}
+
+export function count_inversions_insertion(A) {
+  let inversionCount = 0;
+
+  for (let i = 1; i < A.length; i++) {
+    // Look through all previous items until beginning of array...
+    for (let j = i - 1; j > -1; j--) {
+      // and compare each item with the one ahead of it.
+      if (A[j + 1] < A[j]) {
+        // Swap via destructuring.
+        [A[j + 1], A[j]] = [A[j], A[j + 1]];
+
+        inversionCount = inversionCount + 1;
+      }
+    }
+  }
+
+  return inversionCount > 1000000000 ? -1 : inversionCount;
+}
+
+export function count_inversions_selection(A) {
+  let inversionCount = 0;
+  let min;
+
+  for (let i = 0; i < A.length; i++) {
+    // Min will never be less than i
+    min = i;
+
+    // Pass through all remaining values in the array
+    // and update the min if a value is larger than the min.
+    for (let j = i + 1; j < A.length; j++) {
+      if (A[j] < A[min]) {
+        min = j;
+      }
+    }
+
+    if (min !== i) {
+      [A[i], A[min]] = [A[min], A[i]];
+
+      inversionCount = inversionCount + 1;
+    }
+  }
+
+  return inversionCount > 1000000000 ? -1 : inversionCount;
+}
+
+// Incomplete: Count inversions via merge sort...
+
+function merge({ left, iL }, { right, iR }) {
+  let arr = [];
+  let inversions = iL + iR;
+
+  // Break out of loop if any one of the array gets empty
+  while (left.length && right.length) {
+    // Pick the smaller among the smallest element of left and right sub arrays
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  // Concatenating the leftover elements
+  // (in case we didn't go through the entire left or right array)
+  return { a: [...arr, ...left, ...right], i: inversions };
+}
+
+function mergeSort(A) {
+  const half = A.length / 2;
+
+  // Base case or terminating case
+  if (A.length < 2) {
+    return A;
+  }
+
+  const left = A.splice(0, half);
+
+  return merge(mergeSort(left), mergeSort(A));
+}
+
+export function count_inversions_merge(A) {
+  const sorted = mergeSort(A);
+  console.log(sorted);
+
+  return inversionCount > 1000000000 ? -1 : inversionCount;
+}
--- solutions.test.js
+++ solutions.test.js
@@ -1,31 +1,74 @@
 import Benchmark from "benchmark";
 import chalk from "chalk";
-import Solutions from "./solutions.js";
+import {
+  find_missing_int_reduce,
+  find_missing_int_reduce_with_eject,
+  find_missing_int_forEach,
+  find_missing_int_for,
+  find_missing_int_for_of,
+} from "./solutions.js";
 
 // Returns the input if the test doesn't pass.
-const expect = (fn, input, expected, label) => {
-  const result = fn(input);
+const expect = (fn, expected, ...inputs) => {
+  const result = fn(...inputs);
 
   return result === expected ? null : result;
 };
 
+function arrayUnique(array) {
+  var a = array.concat();
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = i + 1; j < a.length; ++j) {
+      if (a[i] === a[j]) a.splice(j--, 1);
+    }
+  }
+
+  return a;
+}
+
 const runUnitTests = (testCases) =>
-  Solutions.map((test) => ({
-    name: test.name,
-    result: testCases.filter(({ input, validate_with }) =>
-      expect(test, input, validate_with)
-    ),
-  })).forEach(({ name, result }) =>
-    result.length
-      ? console.error(chalk.red(`Something isn't right with ${name}.`))
-      : console.log(chalk.green(`All clear for ${name}`))
+  testCases.reduce(
+    (acc, { input, assert, run }) => {
+      const results = run.reduce(
+        (acc, test) => {
+          const name = test.name;
+          const result = expect(test, assert, input); // Get the result
+
+          result
+            ? acc.fail.push({
+                name: name,
+                cases: [{ input: input, expected: assert, result: result }],
+              })
+            : acc.pass.push(name);
+
+          return acc;
+        },
+        { pass: [], fail: [] }
+      );
+
+      return {
+        pass: acc.pass
+          .concat(results.pass)
+          .filter((el, i, a) => a.indexOf(el) === i), // Remove duplicates
+        fail: acc.fail.concat(results.fail).reduce((bcc, cur) => {
+          // The same test can fail for different input cases.
+          // This reduces each failing case into a single array.
+          const alreadyFailed = bcc.findIndex((el) => el.name === cur.name);
+
+          if (alreadyFailed >= 0) bcc[alreadyFailed].cases.push(...cur.cases);
+
+          return [...bcc, cur];
+        }, []),
+      };
+    },
+    { pass: [], fail: [] }
   );
 
-const runBenchmark = ({ input, validate_with }) => {
+const runBenchmark = ({ input, assert, run }) => {
   const benchmark = new Benchmark.Suite();
 
-  Solutions.forEach((solution) =>
-    benchmark.add(solution.name, () => expect(solution, input, validate_with))
+  run.forEach((solution) =>
+    benchmark.add(solution.name, () => expect(solution, assert, input))
   );
 
   benchmark
@@ -42,43 +85,180 @@ const runBenchmark = ({ input, validate_with }) => {
     .run();
 };
 
-const benchmarkTestCase = {
+const find_missing_int = [
+  find_missing_int_reduce,
+  find_missing_int_reduce_with_eject,
+  find_missing_int_forEach,
+  find_missing_int_for,
+  find_missing_int_for_of,
+];
+
+const benchmark_find_missing_int = {
   // A big array, which won't eject with 1.
   input: Array.from({ length: 456789 }, (_, i) => i - 123),
-  validate_with: 456789 - 123,
+  assert: 456789 - 123,
+  run: find_missing_int,
 };
 
 const unitTestCases = [
   {
     input: [1, 3, 6, 4, 1, 2],
-    validate_with: 5,
+    assert: 5,
+    run: find_missing_int,
   },
   {
     input: [1, 2, 3],
-    validate_with: 4,
+    assert: 4,
+    run: find_missing_int,
   },
   {
     input: [0, 3, 5],
-    validate_with: 1,
+    assert: 1,
+    run: find_missing_int,
   },
   {
     input: [-1, -3],
-    validate_with: 1,
+    assert: 1,
+    run: find_missing_int,
   },
   {
     input: [8, -1, 7],
-    validate_with: 1,
+    assert: 1,
+    run: find_missing_int,
   },
   {
     // A bigger array, starting at 20.
     input: Array.from({ length: 1000 }, (_, i) => i + 20),
-    validate_with: 1,
+    assert: 1,
+    run: find_missing_int,
   },
-  benchmarkTestCase,
+  // benchmark_find_missing_int,
 ];
 
 // Runs all solutions using multiple test cases.
-runUnitTests(unitTestCases);
+const results = runUnitTests(unitTestCases);
+
+results.pass.forEach((name) =>
+  console.log(chalk.green(`All clear for ${name}`))
+);
+
+results.fail.forEach(({ name, cases }) => {
+  return console.error(
+    chalk.red(
+      `Something isn't right with ${chalk.blue.bgWhite.bold(name)}.`,
+      `\n It failed under the following conditions:`,
+      cases.map(
+        ({ input, expected, result }) =>
+          `\n When testing with ${chalk.cyan.bgWhite(
+            input
+          )}, expected ${chalk.green.bgWhite(
+            expected
+          )},but returned ${chalk.red.bgWhite(result)}.`
+      )
+    )
+  );
+});
 
 // Runs all solutions using a single test case.
-runBenchmark(benchmarkTestCase);
+runBenchmark(benchmark_find_missing_int);
+
+let assertions;
+
+// =============================================
+//
+// Challenge: Recursively replace all abb with baa
+
+assertions = [
+  { input: "ababb", output: "baaaa" }, // 2x
+  { input: "abbbabb", output: "babaaaa" }, // 3x
+  { input: "aaabab", output: "aaabab" }, // 1x
+];
+
+// =============================================
+//
+// Challenge: Find the largest binary gap length of an integer
+
+assertions = [
+  { input: 1041, output: 5 },
+  { input: 32, output: 0 },
+  { input: 529, output: 4 },
+];
+
+// assertions
+//   .map(({ input, output }) => expect(solution, input, output))
+//   .map(console.log);
+
+// =============================================
+//
+// Challenge: Rotate an array K times
+
+assertions = [
+  { input: { A: [3, 8, 9, 7, 6], K: 3 }, output: [9, 7, 6, 3, 8] },
+  { input: { A: [0, 0, 0], K: 1 }, output: [0, 0, 0] },
+  { input: { A: [1, 2, 3, 4], K: 4 }, output: [1, 2, 3, 4] },
+  { input: { A: [], K: 1 }, output: [] },
+];
+
+// assertions.map(({ input, output }) =>
+//   expect(({ A, K }) => solution(A, K), input, output)
+// );
+
+// =============================================
+//
+// Challenge: Frog stepping
+assertions = [
+  {
+    input: { x: 5, a: [3] },
+    output: -1,
+  },
+  {
+    input: { x: 1, a: [1] },
+  },
+  {
+    input: { x: 2, a: [2, 2, 2, 2, 2] },
+    output: -1,
+  },
+  {
+    input: { x: 5, a: [1, 3, 1, 4, 2, 3, 5, 4] },
+  },
+];
+
+// =============================================
+//
+// Count bounded slices
+
+// =============================================
+//
+// Tree traversal to return maximum height
+
+// =============================================
+//
+// Incomplete: convert integers to strings of As and Bs
+
+const curry = (fn, x) => (y) => fn(x, y);
+// const curriedSolution = curry(solution, 20);
+
+// a = 5 / 5 'a's
+// b = 3 / 3 'b's
+// return "aabaabab"
+
+// console.log(expect(curriedSolution, "", 17));
+
+// =============================================
+//
+// Inversion counting
+
+//  A[0] = -1 A[1] = 6 A[2] = 3
+//  A[3] =  4 A[4] = 7 A[5] = 4
+//
+// there are four inversions:
+// (1,2)  (1,3)  (1,5)  (4,5)
+// so the function should return 4.
+
+// [-1, 6, 3, 4, 7, 4] // Starting array
+// [-1, 3, 6, 4, 7, 4] // (1, 2) [6, 3]
+// [-1, 3, 4, 6, 7, 4] // (1, 3) [6, 4]
+// [-1, 3, 4, 4, 7, 6] // (1, 5) [6, 7]
+// [-1, 3, 4, 4, 6, 7] // (4, 5) [7, 4]
+
+// console.log(expect(_solution, 4, [-1, 6, 3, 4, 7, 4]));
