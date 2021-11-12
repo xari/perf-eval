cee80626ca8d0a145a02882e916648b5127379ea
xari <hrtanderson@gmail.com>
2021-11-12 23:26:59 +0100

    added new test cases, refactored test runner

--- solutions.js
+++ solutions.js
@@ -1,46 +1,7 @@
-// Codility challenge solutions from my week practice week.
+import R from "ramda";
+
 // Unit tests and benchmark tests can be found in solutions.test.js
 
-// =============================================
-//
-// Find the first integer that isn't in the array.
-
-export function find_missing_int_reduce(A) {
-  return A.filter((n) => n > 0)
-    .sort((a, b) => a - b)
-    .reduce((prev, n, i) => (n === prev ? n + 1 : prev), 1);
-}
-
-export function find_missing_int_reduce_with_eject(A) {
-  return A.filter((n) => n > 0)
-    .sort((a, b) => a - b)
-    .reduce((prev, n, i, arr) => {
-      // This part feels a bit hacky,
-      // but it will early eject from the reduce
-      // if the first element is greater than 1.
-      if (arr[0] !== 1) arr.splice(1);
-
-      return n === prev ? n + 1 : prev;
-    }, 1);
-}
-
-export function find_missing_int_forEach(A) {
-  let x = 1;
-
-  A.filter((n) => n > 0)
-    .sort((a, b) => a - b)
-    .forEach((n) => {
-      if (x < n) {
-        return x;
-      }
-      x = n + 1;
-    });
-
-  return x;
-}
-
-// This solution was taken from the StackOverflow answer below:
-// https://stackoverflow.com/a/56969919
 export function find_missing_int_for(A) {
   A = A.filter((x) => x >= 1).sort((a, b) => a - b);
 
@@ -53,6 +14,22 @@ export function find_missing_int_for(A) {
     }
     x = A[i] + 1;
   }
+
+  return x;
+}
+
+// Same thing as above, but using slightly more elegant syntax.
+export function find_missing_int_forEach(A) {
+  let x = 1;
+
+  A.filter((n) => n > 0)
+    .sort((a, b) => a - b)
+    .forEach((n) => {
+      if (x < n) {
+        return x;
+      }
+      x = n + 1;
+    });
 
   return x;
 }
@@ -72,326 +49,57 @@ export function find_missing_int_for_of(A) {
   return x;
 }
 
-// =============================================
-//
-// Recursively replace all abb with baa
-
-export function recursively_regex(S) {
-  return S.match("abb") === null ? S : solution(S.replace(/abb/g, "baa"));
+// Slow reduce with unnecessary filter in the chain.
+export function basic_chained_reduce(A) {
+  return A.filter((n) => n > 0)
+    .sort((a, b) => a - b)
+    .reduce((prev, n) => (n === prev ? n + 1 : prev), 1);
 }
 
-export function while_replace_regex(S) {
-  let newS = S;
-
-  while (newS.match("abb")) {
-    newS = newS.replace(/abb/g, "baa");
-  }
-
-  return newS;
+// Removes the filter step
+// Looks like crap, but simply encorporates the check from the filter into a ternery that returns the accumulator if n < 0
+export function optimized_reduce(A) {
+  return A.sort((a, b) => a - b).reduce(
+    (prev, n) => (n > 0 ? (n === prev ? n + 1 : prev) : prev),
+    1
+  );
 }
 
-// =============================================
-//
-// Find the largest binary gap length of an integer
+// This one feels a bit hacky,
+// but it will early eject from the reduce
+// if the first element is greater than 1.
+export function optimized_reduce_with_eject(A) {
+  return A.sort((a, b) => a - b).reduce((prev, n, i, arr) => {
+    if (arr[0] > 1) arr.splice(1);
 
-export function find_gap_length(N) {
-  const gapLengths = (N >>> 0) // Convert to binary
-    .toString(2) // Convert to binary
-    .replace(/^0+|0+$/g, "") // Remove 0s on either end
-    .split("1") // get the binary gaps (just the 0s)
-    .filter(Boolean) // Remove the empty strings
-    .map((gap) => gap.length); // (ex. "000" => 3)
-
-  return gapLengths.length
-    ? gapLengths.reduce((acc, cur) => (cur > acc ? cur : acc))
-    : 0;
+    return n > 0 ? (n === prev ? n + 1 : prev) : prev;
+  }, 1);
 }
 
-// =============================================
-//
-// Rotate an array K times
-
-export function rotate_array(A, K) {
-  if (A.length > 0) {
-    for (let i = 0; i < K; i++) {
-      A.unshift(A.pop());
-    }
-  }
-
-  return A;
-}
-
-// =============================================
-//
-// Frog step... (How long until all steps appear?)
-
-export function find_earliest_completion(X, A) {
-  const all_steps = Array.from({ length: X }, (_, i) => i + 1);
-
-  const sorted = A.map((n, i) => ({ n: n, i: i }))
-    .filter((step) => step.n <= X)
-    .sort((step1, step2) => step2.i - step1.i); // Sort by reversed index order... This reversed order is importand because the way that we create the unique values array below will simply look for the last instance of the value in the array.
-
-  const unique = [...new Map(sorted.map((item) => [item["n"], item])).values()];
-
-  return all_steps.every(
-    (step) => unique.map((withI) => withI.n).indexOf(step) !== -1
-  )
-    ? unique.reduce((acc, step) => (step.n === X ? step.i : acc))
-    : -1;
-}
-
-// =============================================
-//
-// Count the bounded slices of an array... This solution is O(N ** 3)
-
-export function count_bounded_slices(K, A) {
-  const bounded_slices = [];
-
-  for (let i = 0; i < A.length; i++) {
-    let j = i + 1;
-
-    while (j <= A.length) {
-      const slice = A.slice(i, j);
-      const min = Math.min(...slice);
-      const max = Math.max(...slice);
-
-      if (Math.abs(min - max) <= K) {
-        bounded_slices.push([i, j]);
-      } else {
-        break;
-      }
-
-      j++;
-    }
-  }
-
-  return bounded_slices.length;
-}
-
-// =============================================
-//
-// Tree traversal to return maximum tree height
-
-export function get_tree_height(T) {
-  const queue = T == null ? [] : [{ t: T, i: 0 }];
-  const heights = [];
-
-  while (queue.length) {
-    const current = queue.shift();
-
-    heights.push(current.i);
-
-    if (current.t.l) {
-      queue.push({ t: current.t.l, i: current.i + 1 });
-    }
-
-    if (current.t.r) {
-      queue.push({ t: current.t.r, i: current.i + 1 });
-    }
-  }
-
-  return T == null ? -1 : Math.max(...heights);
-}
-
-// =============================================
-//
-// Incomplete: convert integers to strings of As and Bs
-// This one has an issue with the rounding.... It doesn't account for uneven leftover amounts of letters... It should be fixed to be able to concat either more or less than the default number of letters, depending on how many of the other numbers are left to be popped.
-
-export function get_a_b_string(A, B) {
-  const As = "a".repeat(A).split("");
-  const Bs = "b".repeat(B).split("");
-  const letters = A >= B ? [...As, ...Bs] : [...Bs, ...As];
-  const spliceThisMany = Math.min(
-    2,
-    Math.round(Math.max(A, B) / Math.min(A, B))
+export function ramda_reduce(A) {
+  const transformer = R.compose(
+    R.reduce((prev, n) => (n === prev ? n + 1 : prev), 1),
+    R.sort((a, b) => a - b),
+    R.filter((n) => n > 0)
   );
 
-  let resultString = "";
-
-  while (letters.length) {
-    resultString = resultString.concat(
-      letters.splice(0, spliceThisMany).join("")
-    );
-
-    const popped = letters.pop();
-
-    // Check first in case there aren't any leftover letters.
-    popped ? (resultString = resultString.concat(popped)) : null;
-  }
-
-  return resultString;
+  return transformer(A);
 }
 
-// =============================================
-//
-// Inversion counting
+export function optimized_ramda_reduce(A) {
+  const transformer = R.compose(
+    R.reduce((prev, n) => (n > 0 ? (n === prev ? n + 1 : prev) : prev), 1),
+    R.sort((a, b) => a - b)
+  );
 
-export function count_inversions_bubble(A) {
-  let inversionCount = 0;
-  let clear = false;
-
-  while (clear !== true) {
-    clear = true;
-
-    for (let i = 0; i < A.length; i++) {
-      const slice = A.slice(i, i + 2);
-
-      if (slice[0] > slice[1]) {
-        A.splice(i, 2, ...slice.reverse());
-        clear = false;
-        inversionCount = inversionCount + 1;
-      }
-    }
-  }
-
-  let i = 0;
-  let queue = [A.slice(i, i + 2)];
-
-  while (queue.length) {
-    if (slice[0] > slice[1]) {
-      A.splice(i, 2, ...slice.reverse());
-    }
-
-    i++;
-    queue.push([A.slice(i, i + 2)]);
-  }
-
-  return inversionCount > 1000000000 ? -1 : inversionCount;
+  return transformer(A);
 }
 
-export function count_inversions_bubble_queue(A) {
-  let inversionCount = 0;
-  let i = 0;
-  let queue = [A.slice(i, i + 2)];
-
-  while (queue.length) {
-    if (slice[0] > slice[1]) {
-      A.splice(i, 2, ...slice.reverse());
-    }
-
-    i++;
-    queue.push([A.slice(i, i + 2)]);
-  }
-
-  return inversionCount > 1000000000 ? -1 : inversionCount;
-}
-
-export function count_inversions_insertion(A) {
-  let inversionCount = 0;
-
-  for (let i = 1; i < A.length; i++) {
-    // Look through all previous items until beginning of array...
-    for (let j = i - 1; j > -1; j--) {
-      // and compare each item with the one ahead of it.
-      if (A[j + 1] < A[j]) {
-        // Swap via destructuring.
-        [A[j + 1], A[j]] = [A[j], A[j + 1]];
-
-        inversionCount = inversionCount + 1;
-      }
-    }
-  }
-
-  return inversionCount > 1000000000 ? -1 : inversionCount;
-}
-
-export function count_inversions_selection(A) {
-  let inversionCount = 0;
-  let min;
-
-  for (let i = 0; i < A.length; i++) {
-    // Min will never be less than i
-    min = i;
-
-    // Pass through all remaining values in the array
-    // and update the min if a value is larger than the min.
-    for (let j = i + 1; j < A.length; j++) {
-      if (A[j] < A[min]) {
-        min = j;
-      }
-    }
-
-    if (min !== i) {
-      [A[i], A[min]] = [A[min], A[i]];
-
-      inversionCount = inversionCount + 1;
-    }
-  }
-
-  return inversionCount > 1000000000 ? -1 : inversionCount;
-}
-
-// Incomplete: Count inversions via merge sort...
-
-function merge({ left, iL }, { right, iR }) {
-  let arr = [];
-  let inversions = iL + iR;
-
-  // Break out of loop if any one of the array gets empty
-  while (left.length && right.length) {
-    // Pick the smaller among the smallest element of left and right sub arrays
-    if (left[0] < right[0]) {
-      arr.push(left.shift());
-    } else {
-      arr.push(right.shift());
-    }
-  }
-
-  // Concatenating the leftover elements
-  // (in case we didn't go through the entire left or right array)
-  return { a: [...arr, ...left, ...right], i: inversions };
-}
-
-function mergeSort(A) {
-  const half = A.length / 2;
-
-  // Base case or terminating case
-  if (A.length < 2) {
-    return A;
-  }
-
-  const left = A.splice(0, half);
-
-  return merge(mergeSort(left), mergeSort(A));
-}
-
-export function count_inversions_merge(A) {
-  const sorted = mergeSort(A);
-  console.log(sorted);
-
-  return inversionCount > 1000000000 ? -1 : inversionCount;
-}
-
-// const lodash = require("lodash")
-//
-// const handleJSON = lodash.throttle((jsonString, saveToFile) => {
-//     let {username, filename, ...restJSON} = JSON.parse(jsonString);
-//
-//     if (username && username.length) username = username.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g,"_");
-//     if (filename && filename.length) filename = filename.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g,"_");
-//
-//     return username.length && filename.length
-//     ? saveToFile(username, filename, JSON.stringify(restJSON))
-//     : null;
-// }, 1000);
-
-// module.exports = handleJSON
-
-export function solution(S) {
-  const balloon = Array.from("BALLOON");
-
-  let letters = Array.from(S.toUpperCase()); // Added case coercion... just in case ;-D
-  let i = 0;
-
-  while (balloon.every((letter) => letters.includes(letter))) {
-    for (let j = 0; j < balloon.length; j++) {
-      letters.splice(letters.indexOf(balloon[j]), 1); // Originally forgot the [j]
-    }
-
-    i++;
-  }
-
-  return i;
+export function ramda_transduce(A) {
+  return R.transduce(
+    R.filter((n) => n > 0),
+    (prev, n) => (n === prev ? n + 1 : prev),
+    1,
+    R.sort((a, b) => a - b, A)
+  );
 }
--- solutions.test.js
+++ solutions.test.js
@@ -1,168 +1,121 @@
 import Benchmark from "benchmark";
+import R from "ramda";
 import chalk from "chalk";
-import {
-  find_missing_int_reduce,
-  find_missing_int_reduce_with_eject,
-  find_missing_int_forEach,
-  find_missing_int_for,
-  find_missing_int_for_of,
-} from "./solutions.js";
-
-// A transducer takes a reducer and returns a reducer...
-// Gets the results of the running each function in the fn array against the provided assertion values.
-// ... and then passes them along to the next reducer, whatever it may be...
-// Remember: the accumulator values will be in the form that the deepest nested reducer returns them in.
-const getResultsTransducer = (nextReducer) => (acc, cur) => {
-  const { fn, assert, input } = cur;
-
-  const results = fn.map((x) => {
-    const result = x(input) === assert ? true : x(input);
-    const pass = result === true ? true : false;
-
-    return {
-      name: x.name,
-      pass,
-      results: [
-        {
-          assert,
-          input,
-          result,
-        },
-      ],
-    };
-  });
-
-  // Always consider what the next reducer should exepct as a current value.
-  // Very important! Remember to pass-on the accumulator as well!
-  return nextReducer(acc, results);
-};
-
-// Shortens the array of results by combining results from testing the same functions more than once.
-const condenseResultsTransducer = (nextReducer) => (acc, cur) => {
-  // set intialValue to the accumulator
-  const condencedResults = cur.reduce((x, y) => {
-    const { name, pass, results } = y;
-    const existing = acc.findIndex((x) => x.name === name);
-
-    return existing !== -1
-      ? [
-          ...x.slice(0, existing),
-          Object.assign(x[existing], {
-            results: x[existing].results.concat(results),
-          }),
-          ...x.slice(existing + 1),
-        ]
-      : x.concat({ name, pass, results });
-  }, acc);
-
-  return nextReducer(condencedResults, cur);
-};
-
-const find_missing_int = [
-  find_missing_int_reduce,
-  find_missing_int_reduce_with_eject,
-  find_missing_int_forEach,
-  find_missing_int_for,
-  find_missing_int_for_of,
-];
+import * as solutions from "./solutions.js";
 
 const benchmark_find_missing_int = {
-  // A big array, which won't eject with 1.
-  fn: find_missing_int,
+  // A big array, which shouldn't eject with 1.
   assert: 456789 - 123,
   input: Array.from({ length: 456789 }, (_, i) => i - 123),
 };
 
 const testCases = [
   {
-    fn: find_missing_int,
     assert: 5,
     input: [1, 3, 6, 4, 1, 2],
   },
   {
-    fn: find_missing_int,
     assert: 4,
     input: [1, 2, 3],
   },
-  // {
-  //     fn: [() => null],
-  //     assert: 1,
-  //     input: 2,
-  //   },
   {
-    fn: find_missing_int,
     assert: 1,
     input: [0, 3, 5],
   },
   {
-    fn: find_missing_int,
     assert: 1,
     input: [-1, -3],
   },
   {
-    fn: find_missing_int,
     assert: 1,
     input: [8, -1, 7],
   },
-  {
-    // A bigger array, starting at 20.
-    fn: find_missing_int,
-    assert: 1,
-    input: Array.from({ length: 1000 }, (_, i) => i + 20),
-  },
-  benchmark_find_missing_int,
+  //   {
+  //     // A bigger array, starting at 20.
+  //     assert: 1,
+  //     input: Array.from({ length: 1000 }, (_, i) => i + 20),
+  //   },
+  // benchmark_find_missing_int,
 ];
 
-// Takes a function to pass the accumulator to... Maybe this is unnecessary....
-const foldReducer = (acc, cur) => {
-  return acc;
+const getResult = (fn, { assert, input }) => {
+  const result = fn(input);
+  const pass = result === assert ? true : false;
+  const data = [
+    {
+      assertion: assert,
+      input,
+      result,
+    },
+  ];
+
+  return {
+    name: fn.name,
+    pass,
+    data,
+  };
 };
 
-// Will tell us which functions pass or fail, and the results vs. expectations for each.
-const results = testCases.reduce(
-  // get the results
-  getResultsTransducer(
-    // pivot longer
-    condenseResultsTransducer(
-      // fold it down
-      foldReducer
-    )
-  ),
-  []
+const condenseResults = (acc, { name, pass, data }) => {
+  const existing = acc.findIndex((x) => x.name === name);
+
+  if (existing !== -1) {
+    pass = acc[existing].pass === false ? false : pass;
+    data = [...acc[existing].data, ...data];
+  }
+
+  const result = {
+    name,
+    pass,
+    data,
+  };
+
+  return existing !== -1
+    ? [...acc.slice(0, existing), result, ...acc.slice(existing + 1)]
+    : acc.concat(result);
+};
+
+let results = R.chain(
+  (fn) => R.map((testCase) => getResult(fn, testCase), testCases),
+  Object.values(solutions)
 );
 
-results.forEach(({ name, pass, results }) => {
-  console.log(
-    pass
-      ? chalk.green(`All clear for ${name}`)
-      : console.error(
-          `Something isn't right with ${chalk.blue.bgWhite.bold(name)}.`,
-          `\n It failed under the following conditions:`,
-          chalk.red(
-            results.map(({ assert, input, result }) =>
-              result !== true
-                ? `\n When testing with ${chalk.cyan.bgWhite(
-                    input
-                  )}, expected ${chalk.green.bgWhite(
-                    expected
-                  )}, but returned ${chalk.red.bgWhite(result)}`
-                : null
-            )
+results = R.reduce(condenseResults, [], results);
+
+results.forEach(({ name, pass, data }) => {
+  pass
+    ? console.log(chalk.green(`All clear for ${name}`))
+    : console.error(
+        `Something isn't right with ${chalk.blue.bgWhite.bold(name)}.`,
+        `\n It failed under the following conditions:`,
+        chalk.red(
+          data.map(
+            ({ assertion, input, result }) =>
+              `\n When testing with ${chalk.cyan.bgWhite(
+                input
+              )}, expected ${chalk.green.bgWhite(
+                assertion
+              )}, but returned ${chalk.red.bgWhite(result)}`
           )
         )
-  );
+      );
 });
 
 // Runs all solutions using a single large array test case.
-const initBenchmark = ({ fn, input }) => {
+const initBenchmark = (solutions, { input }) => {
   const benchmark = new Benchmark.Suite();
 
-  fn.forEach((solution) => benchmark.add(solution.name, () => solution(input)));
+  solutions.forEach((solution) =>
+    benchmark.add(solution.name, () => solution(input))
+  );
 
   return benchmark;
 };
 
-const benchmark = initBenchmark(benchmark_find_missing_int);
+const benchmark = initBenchmark(
+  Object.values(solutions),
+  benchmark_find_missing_int
+);
 
 benchmark
   .on("cycle", function (event) {
